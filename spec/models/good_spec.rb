bc0060b09c6696d91e05c323dcf04b00069870aa
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2013-07-26 23:25:41 +0400

    Update good_spec.rb

--- spec/models/good_spec.rb
+++ spec/models/good_spec.rb
@@ -1,40 +1,26 @@
 require 'spec_helper'
 
 describe Good do
-
-  before do
-  	 @good = Good.new(name: "Example", price: "123") 
-  end
-
-  subject { @good }
+  subject { Good.new(name: "Example", price: 123)  }
 
   it { should respond_to(:name) }
   it { should respond_to(:price) }
-
   it { should be_valid }
 
-  describe "when name is not present" do
-  	before do
-  	 @good = Good.new(name: " ", price: " ") 
-  	end
-   
-    it { should_not be_valid }
-	end
-
-	describe "when name is already taken" do
-		before do
-			record_with_same_name = @good.dup
-			record_with_same_name.save
-		end
-
-		it { should_not be_valid }
-	end
-
-	describe "when price < 0" do
-		before do
-			@good =Good.new(name: "Example", price: "-1")
-		end
-
-		it { should_not be_valid}
-	end
+  it 'should not be valid if name is not present' do	
+    subject.name = ''
+    subject.should_not be_valid
+  end
+  
+  it 'should not be valid if name already taken' do	
+    subject.save
+    good = Good..new(name: "Example", price: 123)
+    good.should_not be_valid 
+  end
+  
+  it 'price should be valid only if greater than 0' do
+    subject.price = -1
+    subject.should_not be_valid    
+  end
+  
 end
