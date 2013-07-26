1561b1aeea7a3b1e3a269b04d4e24f1f3ae41d8f
Victor Yunevich <v.t.g.m.b.x@gmail.com>
2013-07-26 23:31:43 +0400

    Update good.rb

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,10 +1,6 @@
 class Good < ActiveRecord::Base
  
-  validates_uniqueness_of :name
-
-  validates_presence_of :name, :price
-  validates :price, :numericality => { only_integer: true, :greater_then => 0 }
+  validates :name, presence: true, uniqueness: true
+  validates :price, numericality: { greater_then: 0 }  
   
-
- 
 end
